6a418ddf5acffa62c4b79da4cf9ef3015fefe3ee
Yuyang Wang <yw2545@cornell.edu>
2025-08-28 14:46:56 -0700

    update

---
+++ src/theme/DocItem/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '@theme-original/DocItem/Footer';
+import Comments from "../../../components/Comments";
+
+export default function FooterWrapper(props) {
+    return (
+        <>
+            <Footer {...props} />
+            <br/>
+            <hr/>
+
+            <Comments/>
+        </>);
+}
