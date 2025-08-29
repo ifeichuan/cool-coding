3b24a02ce171d0742e30919d6d94a014fa1f91a1
Yuyang Wang <yw886@outlook.com>
2025-08-28 17:57:36 -0700

    Add Footer text to Footer/index.js

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Footer from '@theme-original/DocItem/Footer';
-import Comments from "../../../components/Comments";
+import Comments from '../../../components/Comments';
 
 export default function FooterWrapper(props) {
     return (
         <>
             <Footer {...props} />
-            <br/>
-            <hr/>
 
-            <Comments/>
-        </>);
+            <div style={{ marginTop: '2rem', paddingTop: '1rem', borderTop: '1px solid #e0e0e0' }}>
+                <p style={{ fontSize: '0.95rem', lineHeight: 1.6 }}>
+                    💡 如果你在本站发现错误或笔误，欢迎点击 <strong>“编辑此页”</strong> 来修改。
+                    你的反馈对我们非常重要，非常感谢！🙏
+                </p>
+            </div>
+
+            <Comments />
+        </>
+    );
 }
